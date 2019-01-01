b8c3c2216a7e96e86c9df658c702929fcb836822
Tomer Keren <tomer.keren.dev@gmail.com>
2019-01-02 00:08:05 +0200

    Rename files to be consistent with smell symbols

--- good_smell/smells/__init__.py
+++ good_smell/smells/__init__.py
@@ -1,5 +1,5 @@
-from .range_len_fix import RangeLenSmell
+from .range_len import RangeLenSmell
 from .nested_for import NestedFor
-from .filter import FilterIterator
+from .iterator_filter import FilterIterator
 
 implemented_smells = (RangeLenSmell, NestedFor, FilterIterator)
