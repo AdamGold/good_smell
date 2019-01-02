2ebd79582fcdf75a6cf44750110002888dd5414a
Tomer Keren <tomer.keren.dev@gmail.com>
2019-01-02 02:43:54 +0200

    WIP:Start implementing iterator_map

--- good_smell/smells/__init__.py
+++ good_smell/smells/__init__.py
@@ -1,5 +1,6 @@
 from .range_len import RangeLenSmell
 from .nested_for import NestedFor
 from .iterator_filter import FilterIterator
+from .iterator_map import IteratorMap
 
 implemented_smells = (RangeLenSmell, NestedFor, FilterIterator)
